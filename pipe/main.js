d23d91832bda504bb7c6c8c624ae781afd7a3681
Esteban Andres Murcia Acuña <estebandres@proton.me>
2023-09-09 09:45:12 -0500

    probando lo de pipe

--- pipe/main.js
+++ pipe/main.js
@@ -1,61 +1,10 @@
 // Función para cargar el modelo de manera asíncrona
 async function loadModel() {
-    // Fetch the binary weights file (group1.bin)
-fetch('path/to/group1.bin') // Replace with the correct path or URL to your binary file
-.then(response => {
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return response.arrayBuffer(); // This converts the response to an ArrayBuffer
-})
-.then(arrayBuffer => {
-  // Handle the binary data in the 'arrayBuffer'
-  // For example, you can use it to create a TypedArray (e.g., Uint8Array)
-  const uint8Array = new Uint8Array(arrayBuffer);
+    const tfn = require('@tensorflow/tfjs-node');
+    const modelPath = 'model.json';
+    const handler = tfn.io.fileSystem(modelPath);
 
-  // Load the model architecture (model.json)
-  tf.loadLayersModel('path/to/model.json') // Replace with the correct path or URL to your model.json
-    .then(model => {
-      // Manually assign the binary weights to the model's weights
-      const weightsManifest = [
-        {
-          paths: ['group1.bin'],
-          weights: [
-            { name: 'conv2d/kernel', shape: [3, 3, 1, 32], dtype: 'float32' },
-            { name: 'conv2d/bias', shape: [32], dtype: 'float32' },
-            { name: 'conv2d_1/kernel', shape: [3, 3, 32, 64], dtype: 'float32' },
-            { name: 'conv2d_1/bias', shape: [64], dtype: 'float32' },
-            { name: 'dense/kernel', shape: [1600, 256], dtype: 'float32' },
-            { name: 'dense/bias', shape: [256], dtype: 'float32' },
-            { name: 'dense_1/kernel', shape: [256, 7], dtype: 'float32' },
-            { name: 'dense_1/bias', shape: [7], dtype: 'float32' },
-          ],
-        },
-      ];
-
-      // Load the binary weights into the model
-      for (const manifest of weightsManifest) {
-        const paths = manifest.paths;
-        const weights = manifest.weights;
-
-        for (let i = 0; i < paths.length; i++) {
-          const weightData = uint8Array; // Use the entire binary data
-          model.weights.find(weight => weight.originalName === weights[i].name).read(weightData);
-        }
-      }
-
-      // Now, 'model' contains the architecture and loaded binary weights
-      // You can use 'model' for inference as usual
-    })
-    .catch(error => {
-      console.error('Error loading model:', error);
-    });
-})
-.catch(error => {
-  console.error('Fetch error:', error);
-});
-
-    //const model = await tf.loadLayersModel('model.json');
+    const model = await tf.loadLayersModel(handler);
     return model;
 }
 
