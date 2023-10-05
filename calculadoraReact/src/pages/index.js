5b33a438908908f59926dadbe87467ed1bbb4d1c
Esteban Andres Murcia Acuña <estebandres@proton.me>
2023-10-05 15:02:40 -0500

    Updates

--- calculadoraReact/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./home";
-import LongitudOnda from "./longitud-onda"
-import LimiteShannon from "./limite-shannon"
-import RuidoTermico from "./ruido-termico"
-import VoltajeRuido from "./voltaje-ruido"
-import FactorRuido from "./factor-ruido"
-export {Home,LongitudOnda,LimiteShannon,RuidoTermico,VoltajeRuido,FactorRuido}
